63ebc8b38817c45c307c331686c9ee1a44a47db0
Chirag <takchirag828@gmail.com>
2022-10-14 23:23:15 +0530

    home page development

--- src/Home/pages/Home.js
+++ src/Home/pages/Home.js
@@ -3,6 +3,17 @@ import '../css/Home.css';
 function Home() {
     return (
       <>
+      <div><h1>Navbar</h1></div>
+        <div className='title-main-container'>
+          <div className='title-div'>
+            <h1 className='title-header'>Projects that you Love</h1>
+            <button className='start-button'>Get in Touch</button>
+          </div>
+          <img
+            src="https://thumbs.dreamstime.com/b/projects-concept-black-chalkboard-d-rendering-handwritten-top-view-office-desk-lot-business-office-supplies-79906734.jpg"
+            className='title-image'
+          />
+        </div>
         <div className='main-container'>
             <Card title="Website" imageLink="https://sites.udel.edu/njimenez/files/2018/05/how-to-create-a-website-feature-image-e1496943224192-1yzdyp6.jpg"/>
             <Card title="App" imageLink="https://www.webhopers.com/wp-content/uploads/2022/02/app-development-company-haridwar.jpeg"/>
