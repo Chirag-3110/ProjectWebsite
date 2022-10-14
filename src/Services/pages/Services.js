6848a72661d61beedad04b4415fff875eabd95a4
abhijan2402 <abhishek.jangid643@gmail.com>
2022-10-14 11:47:55 +0530

    Addign services

--- src/Services/pages/Services.js
+++ src/Services/pages/Services.js
@@ -1,13 +1,29 @@
-import {Link} from 'react-router-dom'
+import "./Services.css"
 function About() {
-    return (
-      <>
-        <main>
-          <h2>service</h2>
-        </main>
-        <nav>
-          <Link to="/">Home</Link>
-        </nav>
-      </>
-    );
-}export default About
+  return (
+    <>
+      <div className='Header'>
+        <div>
+
+          <div className='MainCard'>
+            <div className='innerMainCard'>
+              <img
+                src="https://bobbyhadz.com/images/blog/react-prevent-multiple-button-clicks/thumbnail.webp"
+                alt="car"
+                className='Image'
+
+              />
+              <h3 className="h3">Website</h3>
+              <p className="para">Website is the designed
+              </p>
+              <button className="btn">Book Now</button>
+            </div>
+
+          </div>
+          <div>hi</div>
+        </div>
+
+      </div>
+    </>
+  );
+} export default About
