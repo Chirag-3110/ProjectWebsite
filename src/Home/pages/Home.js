235349ee1a9b5b8f689d206fbf43bb70a464f65e
Chirag <takchirag828@gmail.com>
2022-10-14 11:50:18 +0530

    home page

--- src/Home/pages/Home.js
+++ src/Home/pages/Home.js
@@ -1,13 +1,13 @@
-import {Link} from 'react-router-dom'
+import Card from '../components/Card';
+import '../css/Home.css';
 function Home() {
     return (
       <>
-        <main>
-          <h2>Homepage</h2>
-        </main>
-        <nav>
-          <Link to="/service">service</Link>
-        </nav>
+        <div className='main-container'>
+            <Card title="Website" imageLink="https://sites.udel.edu/njimenez/files/2018/05/how-to-create-a-website-feature-image-e1496943224192-1yzdyp6.jpg"/>
+            <Card title="App" imageLink="https://www.webhopers.com/wp-content/uploads/2022/02/app-development-company-haridwar.jpeg"/>
+            <Card title="Portfolio" imageLink="https://img.freepik.com/free-vector/hand-drawn-portfolio-template_52683-79647.jpg?w=2000"/>
+        </div>
       </>
     );
 }
