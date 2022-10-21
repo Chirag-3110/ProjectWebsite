fb52f7533ce67eca6dc22c8aa395445cd53a7f73
abhijan2402 <abhishek.jangid643@gmail.com>
2022-10-21 22:57:37 +0530

    Adddign feedback apge'

--- src/Feedback/Pages/Feedback.js
+++ src/Feedback/Pages/Feedback.js
@@ -9,14 +9,14 @@ function Feedback() {
                 <button className="contact-btn">Feedback</button>
             </div>
             <div>
-                <TextField id="outlined-basic" label="Name" variant="filled" size='small' color="warning" style={{ width: "30%", borderRadius: "10px" }} />
+                <TextField id="outlined-basic" label="Name" variant="filled" size='small' color="warning" style={{ width: "20%", borderRadius: "10px", marginRight: "10px" }} />
+                <TextField id="outlined-basic" label="Email" variant="filled" size='small' color="warning" style={{ width: "20%", borderRadius: "10px" }} />
                 <div style={{ margin: "2% 0px" }}>
-                    <TextField id="outlined-basic" label="Email" variant="filled" size='small' color="warning" style={{ width: "30%", borderRadius: "10px" }} />
                 </div>
                 <div style={{ margin: "2% 0px" }}>
-                    <TextField id="outlined-basic" label="Feedback" variant="filled" size='Large' color="warning" rows={10} multiline style={{ width: "30%" }} />
+                    <TextField id="outlined-basic" label="Feedback" variant="filled" size='Large' color="warning" rows={10} multiline style={{ width: "41%" }} />
                 </div>
-                <Button variant="contained" size="large" style={{ backgroundColor: "#FF7A00", width: "30%", fontSize: "20px" }}>Submit</Button>
+                <Button variant="contained" size="large" style={{ backgroundColor: "#FF7A00", width: "20%", fontSize: "20px" }}>Submit</Button>
 
             </div>
         </>
