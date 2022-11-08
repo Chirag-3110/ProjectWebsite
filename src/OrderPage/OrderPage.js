ca7bc1e1cc5d956ba38b8044bdc89ee55df4e2f1
abhijan2402 <abhishek.jangid643@gmail.com>
2022-11-08 17:40:36 +0530

    Updating order Page

--- src/OrderPage/OrderPage.js
+++ src/OrderPage/OrderPage.js
@@ -1,51 +1,56 @@
 import React, { useEffect, useState } from 'react'
 import '../Feedback/Pages/feedback.css';
-import OrderContent from './OrderContent';
 import './OrderPage.css'
 import { db } from "../firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { collection, query, where, getDocs } from "firebase/firestore";
 function OrderPage() {
-    const [orderArray, setOrderArray] = useState([])
+    const [orderDetail, setOrderDetail] = useState([]);
     useEffect(() => {
-        // createNewUser();
+        getOrderData();
     }, [])
-    const createNewUser = async () => {
-        try {
-            const docRef = doc(db, "Orders", "HyYvISaH2CkCFxvA4N2q");
-            const docSnap = await getDoc(docRef);
-            if (docSnap.exists()) {
-                const OArray = docSnap.data();
-                console.log(OArray)
-                setOrderArray(OArray)
-                console.log(orderArray)
-
-            } else {
-                console.log("No such document!");
-            }
-        }
-        catch (error) {
-            console.log(error);
-        }
+    const getOrderData = () => {
+        let resultArray = [];
+        let conditinoOne = where("UserUid", "==", 'uhfhhsj');
+        const baseQuery = query(collection(db, "Orders"), conditinoOne);
+        getDocs(baseQuery).then((res) => {
+            res.forEach((item) => {
+                resultArray.push({ id: item.id, ...item.data() });
+            })
+            setOrderDetail(resultArray)
+        })
     }
     return (
         <>
             <div className="contact-div">
                 <button className="contact-btn">OrderPage</button>
             </div>
-            <div className='OrderMainPage'>
-                <div className='OrderDetailDiv'>
-                    <img src="https://informationq.com/wp-content/uploads/2018/01/WebPage.jpg" className='OrderImg' />
-                </div>
-                <div className='OrderDetailDiv1'>
-                    <p>A meta description is an HTML element that contains a short summary of your page and generates the brief snippet you see underneath a sites title in Googles organic search results. Heres an example of what it usually looks like:</p>
-                </div>
-                <div className='OrderDetailDiv' style={{ marginTop: "1.8%" }}><h3>Price :900</h3></div>
-                <div className='OrderDetailDiv' style={{ marginTop: "1.8%" }}><h3>Status:<h4 style={{ color: "green" }}>Preparing</h4></h3></div>
-            </div>
-            {/* <OrderContent img="https://informationq.com/wp-content/uploads/2018/01/WebPage.jpg" OrderDesc="A meta description is an HTML element that contains a short summary of your page and generates the brief snippet you see underneath a sites title in Googles organic search results. Heres an example of what it usually looks like:" Price="800" Status="Preparing" /> */}
-            <OrderContent img="https://www.ipixtechnologies.com/images/google-lens-image-recognition-apps.jpg" OrderDesc="A meta description is an HTML element that contains a short summary of your page and generates the brief snippet you see underneath a sites title in Googles organic search results. Heres an example of what it usually looks like:" Price="800" Status="Preparing" />
-            <OrderContent img="https://bleazyusa.com/wp-content/uploads/2016/05/mobile-dev-ft-img.png" OrderDesc="A meta description is an HTML element that contains a short summary of your page and generates the brief snippet you see underneath a sites title in Googles organic search results. Heres an example of what it usually looks like" Price="800" Status="Preparing" />
-
+            {
+                orderDetail.length === 0 ? null :
+                    orderDetail.map((item) => (
+                        <div className='OrderMainPage'>
+                            <div className='OrderDetailDiv'>
+                                <h3>ProjectType - {item.projectType}</h3>
+                                <img alt='' src="https://informationq.com/wp-content/uploads/2018/01/WebPage.jpg" className='OrderImg' />
+                            </div>
+                            <div className='OrderDetailDiv1'>
+                                <h3>{item.projectName}-{item.duration}</h3>
+                                <p>{item.description}</p>
+                                <div style={{ display: "flex", flexDirection: "row", margin: "auto", width: "auto", textAlign: "center", justifyContent: "center", }}>
+                                    {
+                                        item.projectTech.map((value) => (
+                                            <>
+                                                <h3 style={{ textAlign: "center", margin: "2% 5%" }}>{value}</h3>
+                                            </>
+                                        ))
+                                    }
+                                </div>
+                            </div>
+                            <div className='OrderDetailDiv' style={{ marginTop: "1.8%" }}><h3>Price :{item.price}</h3> </div>
+                            <div className='OrderDetailDiv' style={{ marginTop: "1.6%" }}><h3>Status-<h4 style={{ color: "green" }}>{item.request}</h4></h3>
+                            </div>
+                        </div>
+                    ))
+            }
         </>
     )
 }
